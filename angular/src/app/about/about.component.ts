09c4af7ae0e6365743423ade0a0be32799355689
AntoineVigneronIsisCastres <antoine.vigneron@etud.univ-jfc.fr>
2023-12-07 22:33:59 +0100

    Added styles + leaderboards

--- angular/src/app/about/about.component.ts
+++ angular/src/app/about/about.component.ts
@@ -9,5 +9,16 @@ import { CommonModule } from '@angular/common';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-
+  users = [
+    { id: 1, username: 'user1' },
+    { id: 2, username: 'user2' },
+    { id: 3, username: 'user3' },
+    { id: 4, username: 'user4' },
+    { id: 5, username: 'user5' },
+    { id: 6, username: 'user6' },
+    { id: 7, username: 'user7' },
+    { id: 8, username: 'user8' },
+    { id: 9, username: 'user9' },
+    { id: 10, username: 'user10' },
+  ]
 }
--- angular/tailwind.config.js
+++ angular/tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blackbean: '#301014',
+        xanthous: '#ffc145',
+        emerald2: '#5dd39e',
+        custombeige: '#fff0d4',
+      },
+    },
   },
   plugins: [require("@tailwindcss/forms")],
 };
